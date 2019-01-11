d6bb73031ef40279369aa7e95cb433c53005db6d
Rufus Nash <rufus.nash@opencredo.com>
2019-01-11 11:19:51 +0000

    Commiting the original empty project.

---
+++ src/test/java/com/opencredo/connect/venafi/tpp/log/VenafiTppLogConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opencredo.connect.venafi.tpp.log;
+
+
+import org.junit.jupiter.api.Test;
+
+public class VenafiTppLogConnectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
