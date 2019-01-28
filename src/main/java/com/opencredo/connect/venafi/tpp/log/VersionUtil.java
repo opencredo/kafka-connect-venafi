c723a3aa67b50e5a2e1d74e0467e91dd1472626e
Rufus Nash <rufus.nash@opencredo.com>
2019-01-28 16:45:43 +0000

    Change per PR added additional log.

--- src/main/java/com/opencredo/connect/venafi/tpp/log/VersionUtil.java
+++ src/main/java/com/opencredo/connect/venafi/tpp/log/VersionUtil.java
@@ -1,15 +1,19 @@
 package com.opencredo.connect.venafi.tpp.log;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.util.Properties;
 
 public class VersionUtil {
+    private static final Logger log = LoggerFactory.getLogger(VersionUtil.class);
     public static String getVersion() {
         final Properties properties = new Properties();
         try {
             properties.load(VersionUtil.class.getClassLoader().getResourceAsStream("application.properties"));
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("Unable to load Project version from application.properties.",e);
         }
         return properties.getProperty("project.version","0.0.1");
 
