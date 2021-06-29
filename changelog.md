071f970afc9502bd3e781dd10615ca84abf9aca2
jbowkett <james@bowkett.info>
2021-06-29 18:43:17 +0000

    JB Temp check-in

---
+++ changelog.md
@@ -0,0 +1,16 @@
+
+0.9.6
+---
+Ensured test and module compatibility with Open JDK 11
+
+Coming soon:
+Minor Logging updates
+Ensured passwords are held private in Kafka logging
+Move to version 1.0?
+Ensure compatibility with Venafi platform 20.1
+Ensure compatibility with Latest Confluent Kafka
+Integration test with in-process Kafka
+
+
+0.9.5 Release
+----
---
+++ src/test/java/com/opencredo/connect/venafi/tpp/log/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.opencredo.connect.venafi.tpp.log;
+
+import org.junit.jupiter.api.Test;
+
+public class IntegrationTest {
+
+
+    @Test
+    void ensureAMessagePlacedOnTheStubEndpointIsReceivedByKafka(){
+        //create a stub endpoint for Venafi
+        //start a background kafka
+        //install the connector into kafka
+        //pump a set of messages through the Venafi endpoint
+        //ensure they are seen on the Kafka topic
+
+    }
+}
